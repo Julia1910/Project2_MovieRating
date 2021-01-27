1178fd46f91e828b0df0b5f8191d49a78abf46ad
Juliya <ulcakopitko@gmail.com>
2021-01-27 13:54:00 +0200

    Init commit

---
+++ src/main/java/com/cursor/MovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.cursor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRatingApplication.class, args);
+    }
+
+}
