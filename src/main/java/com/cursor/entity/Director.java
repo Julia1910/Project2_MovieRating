bba8e489c12ca11ed67ddfb431bdf36bdd40f4a2
Oleg <kandalov.o.a@gmail.com>
2021-02-01 23:48:57 +0200

    Create entity Director; create connections with Movie

---
+++ src/main/java/com/cursor/entity/Director.java
@@ -0,0 +1,20 @@
+package com.cursor.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Directors")
+@Data
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne(mappedBy = "director")
+    List<Movie> movieList;
+}
