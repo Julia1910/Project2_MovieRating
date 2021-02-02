78e1c47f9591141b853d01bf6deeac1b17b98d27
Oleg <kandalov.o.a@gmail.com>
2021-02-02 17:15:13 +0200

    package dao + marker DirectorRepository

---
+++ src/main/java/com/cursor/dao/DirectorRepository.java
@@ -0,0 +1,7 @@
+package com.cursor.dao;
+
+import com.cursor.model.Director;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectorRepository extends JpaRepository<Director, Long>{
+}
