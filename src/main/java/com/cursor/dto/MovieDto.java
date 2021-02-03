281fa1f4a56390766a85bfe56ef3a86b5cf1b261
Juliya <ulcakopitko@gmail.com>
2021-02-03 18:25:32 +0200

    Add test

--- src/main/java/com/cursor/dto/MovieDto.java
+++ src/main/java/com/cursor/dto/MovieDto.java
@@ -1,4 +1,15 @@
 package com.cursor.dto;
 
-public abstract class MovieDto {
+import com.cursor.model.enums.Category;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MovieDto {
+    private String title;
+    private List<Category> category;
+    private String shortDescription;
+    private Double rateValue;
+    private List<DirectorDto> directors;
 }
--- src/test/java/com/cursor/service/ReviewServiceImplTest.java
+++ src/test/java/com/cursor/service/ReviewServiceImplTest.java
@@ -1,9 +1,6 @@
 package com.cursor.service;
 
-import com.cursor.dto.DirectorDto;
-import com.cursor.dto.MovieDto;
 import com.cursor.dto.ReviewDto;
-import com.cursor.model.enums.Category;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,18 +20,51 @@ class ReviewServiceImplTest {
     private ReviewService reviewService;
 
     @Test
-    void add() {
+    void addReview() {
+        ReviewDto reviewDto = new ReviewDto();
+        reviewDto.setLiked(false);
+        reviewDto.setReviewMessage("Bad movie");
+        reviewDto.setMovieId(1L);
+        ReviewDto reviewDto1 = reviewService.add(reviewDto);
+        assertEquals(reviewDto, reviewDto1);
     }
 
     @Test
-    void getById() {
+    void getReviewById() {
+        ReviewDto reviewDto = reviewService.getById(6L);
+        System.out.println(reviewDto);
     }
 
     @Test
     void getAll() {
+        ReviewDto reviewDto = new ReviewDto();
+        reviewDto.setLiked(true);
+        reviewDto.setReviewMessage("Good movie");
+        reviewDto.setMovieId(1L);
+        List<ReviewDto> expectedReviews = new ArrayList<>();
+
+        ReviewDto reviewDto1 = new ReviewDto();
+        reviewDto1.setLiked(false);
+        reviewDto1.setReviewMessage("Bad movie");
+        reviewDto1.setMovieId(1L);
+
+        expectedReviews.add(reviewDto);
+        expectedReviews.add(reviewDto1);
+        expectedReviews.add(reviewDto1);
+
+        List<ReviewDto> getReviews = reviewService.getAll();
+        assertEquals(expectedReviews, getReviews);
     }
 
     @Test
     void remove() {
+        ReviewDto expectedReviewDto = new ReviewDto();
+        expectedReviewDto.setLiked(true);
+        expectedReviewDto.setReviewMessage("Good movie");
+        expectedReviewDto.setMovieId(1L);
+
+        ReviewDto actualReviewDto = reviewService.remove(5l);
+
+        assertEquals(expectedReviewDto, actualReviewDto);
     }
 }
