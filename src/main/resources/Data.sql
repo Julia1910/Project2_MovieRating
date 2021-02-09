9a0b71275ec72156842cd477a2a7a1363cba82de
Oleg <kandalov.o.a@gmail.com>
2021-02-09 18:05:40 +0200

    added 2 file.sql for example

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,8 @@
+INSERT INTO movies (id, title, description)
+VALUES (13, 'Run Forest', 'true story');
+INSERT INTO reviews (id, liked, review_message, movie_id) VALUES (5, false, 'good film', 13);
+INSERT INTO movies (id, title, description)
+VALUES (14, 'Terminator', 'fake story');
+INSERT INTO reviews (id, liked, review_message, movie_id) VALUES (23, false, 'bad movie', 14);
+
+
