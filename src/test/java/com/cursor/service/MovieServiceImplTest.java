ca55eb7913e20fc1b14137d4939dcbb29bc2a9c3
Roman Bielyi <romanbielyi.amor@gmail.com>
2021-02-12 18:16:13 +0200

    Added missed import

--- src/test/java/com/cursor/service/MovieServiceImplTest.java
+++ src/test/java/com/cursor/service/MovieServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
